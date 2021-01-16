53461432d49acccd873a7c68bf4e78589882978e
corporateanon@gmail.com <corporateanon>
2021-01-16 20:31:27 +0200

    2

--- jest.config.js
+++ jest.config.js
@@ -52,10 +52,10 @@ module.exports = {
     // forceCoverageMatch: [],
 
     // A path to a module which exports an async function that is triggered once before all test suites
-    globalSetup: './__tests__/globalSetup.js',
+    // globalSetup: './__tests__/globalSetup.js',
 
     // A path to a module which exports an async function that is triggered once after all test suites
-    globalTeardown: './__tests__/globalTeardown.js',
+    // globalTeardown: './__tests__/globalTeardown.js',
 
     // A set of global variables that need to be available in all test environments
     // globals: {},
---
+++ src/__tests__/example.spec.ts
@@ -0,0 +1,5 @@
+describe('Example', () => {
+    it('should run', () => {
+        expect(2 + 2).toEqual(4);
+    });
+});
