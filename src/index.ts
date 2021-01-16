fcbf195c42182d85f96dd82e486393562a752abe
corporateanon@gmail.com <corporateanon>
2021-01-16 20:19:38 +0200

    init

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export class C {
+    private x = 10;
+    getX = () => this.x;
+    setX = (newVal: number) => { this.x = newVal; }
+}
+
+export const x = new C();
+export const y = { ...{ some: 'value' } };
